f0d7fd5a284db5ca14438e80e2b4b90f32afae72
raeland <133458971+raeland@users.noreply.github.com>
2024-03-26 22:26:20 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,13 @@
+const router = require('express').Router()
 
+router.use('/', require('./swagger'))
+
+//router.get("/", (req, res) => { 
+    //#swagger.tags=['Hello World']
+  // res.send ('Hello World')
+//})
+
+router.use('/inventory', require('./inventory'))
+router.use('/supplier', require('./supplier'))
+
+module.exports = router
