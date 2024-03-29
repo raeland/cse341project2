d0544df436f21b65a76affeeb0bf845d18a4b37d
raeland <rsvarady@gmail.com>
2024-03-28 20:58:07 -0400

    wooHoo

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./data/database');
-const session = require('express-session');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongodb = require('./data/database')
+const session = require('express-session')
+const cors = require('cors')
 
-const port = process.env.PORT || 3000;
-const app = express();
+const port = process.env.PORT || 3000
+const app = express()
 
 app
     .use(bodyParser.json())
@@ -14,30 +14,27 @@ app
         resave: false,
         saveUninitialized: true,
     }))
+
     // This is the basic epxression session ({..}) initialization.
-    .use(passport.initialize())
-    // init passport on every route call.
-    .use(passport.session())
-    // allow passport to use "express-session".
     .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Origin', '*')
         res.setHeader(
             'Access-Control-Allow-Headers',
             'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
         );
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         next();
     })
     .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
     .use(cors({ origin: '*' }))
-    .use('/', require('./routes'));
+    .use('/', require('./routes'))
 
-app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out") });
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out") })
 
 mongodb.initDb((err) => {
     if (err) {
-        console.log(err);
+        console.log(err)
     } else {
-        app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)});
+        app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)})
     }
-});
+})
