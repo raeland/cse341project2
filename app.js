8f239642580ba1832cec3e7e67cacff62adce725
raeland <rsvarady@gmail.com>
2024-03-30 12:13:04 -0400

    from server to app

--- app.js
+++ app.js
@@ -7,21 +7,32 @@ const dotenv = require('dotenv').config()
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    next()
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    next()
-  })
-app.use('/', require('./routes'))
+app
+    .use(bodyParser.json())
+    .use(session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: true,
+    }))
 
-const port = process.env.PORT || 8080
-  
+    // This is the basic epxression session ({..}) initialization.
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+        );
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        next()
+    })
+    .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+    .use(cors({ origin: '*' }))
+    .use('/', require('./routes'))    
+    
 mongodb.initDb((err) => {
-  if (err) {
-      console.log(err);
-  } else {
-      app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)});
-  }
-})
+    if (err) {
+      console.log(err)
+    } else {
+    app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)})
+     }
+    })
--- server.js
+++ server.js
@@ -29,10 +29,10 @@ app
     .use(cors({ origin: '*' }))
     .use('/', require('./routes'))    
     
-   // mongodb.initDb((err) => {
-     //   if (err) {
-       //     console.log(err);
-        //} else {
-          //  app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)});
-       // }
-   // })
+mongodb.initDb((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+    app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)});
+     }
+    })
